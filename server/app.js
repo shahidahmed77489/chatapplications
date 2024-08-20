1b5d620c16be0c483ce9ee27f9a8a254a59cc20d
shahidahmed77489 <ms6992902@gmail.com>
2024-08-20 12:41:46 +0530

    cors-error-solved

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const server = createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST"],
     credentials: true,
   },
@@ -17,7 +17,7 @@ const io = new Server(server, {
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST"],
     credentials: true,
   })
